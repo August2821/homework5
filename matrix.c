42875e2345a4faff9d3ad6102b8889ef3f5743ac
Min <sut4080@gmail.com>
2024-04-11 19:41:24 +0900

    feat: "matrix 기능 추가" print_matrix, addition_matrix, free_matrix, subtraction_matrix, transpose_matrix, multiply_matrix

--- matrix.c
+++ matrix.c
@@ -2,47 +2,132 @@
 #include<stdlib.h>
 #include<time.h>
 
-int print_matrix(){
+int row; //row를 전역변수로 선언
+int column; //column을 전역변수로 선언
+int **num; //num을 전역변수로 선언, 결과값 저장하는 변수
 
-}
-
-int addition_matrix(){
-
-}
-
-int subtraction_matrix(){
-
-}
-
-int transpose_matrix(){
-
-}
-
-int multiply_matrix(){
-
-}
-
-void free_matrix(){
-
-}
+void print_matrix(int **a);
+void addition_matrix(int **a,int **b);
+void free_matrix(int **a,int **b);
+void subtraction_matrix();
+void transpose_matrix();
+void multiply_matrix(int **a,int **b);
 
 int main(){
-    int row,column,**arr,i,j; 
-    scanf("%d %d",&row,&column); //행과 열을 저장할 변수
+    printf("[----- [김민경] [2023041054] -----]\n\n");
 
-    arr=(int**)malloc(sizeof(int)*row); //arr[row] 동적 메모리 할당
+    int **a,**b,i,j;
+    printf("row와 column을 입력하세요: ");
+    scanf("%d %d",&row,&column);
+    printf("\n");
+
+    a=(int**)malloc(sizeof(int)*row);
+    b=(int**)malloc(sizeof(int)*row); //arr[row] 동적 메모리 할당
     for(i=0;i<row;i++){
-        arr[i]=(int*)malloc(sizeof(int)*column);
-    }
-
-    srand((unsigned)time(NULL));
-
-    for(i=0;i<row;i++){
-        for(j=0;j<column;j++){
-            arr[i][j]=(rand()%50)+1;
-            printf("%d ",arr[i][j]);
-        }printf("\n");
+        a[i]=(int*)malloc(sizeof(int)*column);
+        b[i]=(int*)malloc(sizeof(int)*column);
     }
     
+    num=(int**)malloc(sizeof(int)*row); //arr[row] 동적 메모리 할당
+    for(i=0;i<row;i++){
+        num[i]=(int*)malloc(sizeof(int)*column);
+    }
+
+    //값 할당
+    srand((unsigned)time(NULL));
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            a[i][j]=(rand()%50)+1;
+            b[i][j]=(rand()%50)+1; 
+        }
+    }
+
+
+    printf("행렬 A");
+    print_matrix(a);
+
+    printf("행렬 B");
+    print_matrix(b);
+
+    printf("addition_matrix");
+    addition_matrix(a,b); 
+    print_matrix(num);
+
+    printf("subtraction_matrix");
+    subtraction_matrix(a,b); 
+    print_matrix(num);
+
+    printf("transpose_matrix");
+    transpose_matrix(a);
+    print_matrix(num);
+
+    printf("multiply_matrix");
+    multiply_matrix(a,b);
+    print_matrix(num);
+
+    free_matrix(a,b);
     return 0;
+}
+
+void print_matrix(int **a){
+    int i,j;
+    printf("\n===============================\n");
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            printf("%d\t",a[i][j]);
+        }printf("\n");
+    }
+    printf("\n");
+}
+
+void addition_matrix(int **a,int **b){
+    int i,j;
+    
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+           num[i][j]=a[i][j]+b[i][j];
+        }
+    }
+}
+
+void free_matrix(int **a,int **b){
+    int i;
+    for(i=0;i<row;i++){
+        free(num[i]);
+        free(a[i]);
+        free(b[i]);
+    }
+    free(num);
+    free(a);
+    free(b);
+}
+
+void subtraction_matrix(int **a,int **b){
+    int i,j;
+    
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+           num[i][j]=a[i][j]-b[i][j];
+        }
+    }
+}
+
+void transpose_matrix(int **a){
+    int i,j;
+    
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+           num[i][j]=a[j][i];
+        }
+    }
+}
+
+void multiply_matrix(int **a,int **b){
+    int i,j;
+    
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+           num[i][j]=a[i][j]*b[i][j];
+        }
+    }
 }
